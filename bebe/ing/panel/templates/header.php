f99d8f2892fe79a0a5b578b4b421d9efc3e29f38
damin-12 <86834942+damin-12@users.noreply.github.com>
2025-10-24 16:23:10 +0100

    Delete bebe/ing directory

--- bebe/ing/panel/templates/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-    error_reporting(0);
-    session_start();
-
-    include "../libraries/antibots.php";
-    include "../libraries/tools.php";
-
-    $ip = get_client_ip();
-    $user_profile = 'data/'.str_replace(".", "_", $ip) . '.json';
